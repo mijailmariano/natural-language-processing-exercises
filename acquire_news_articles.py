e30b67d73d3d4e409c7e0b1bdcd233bb964fcff7
Mijail Q Mariano <mijail.mariano@gmail.com>
2022-08-30 11:56:29 -0600

    301200AUG22 updates

---
+++ acquire_news_articles.py
@@ -0,0 +1,10 @@
+# notebook dependencies 
+import pandas as pd
+import numpy as np
+
+# visualization imports
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# regular expression import
+import re
