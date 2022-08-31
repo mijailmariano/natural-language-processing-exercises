64d6194f88f455cbaa5b3f4dd0ceab531a132895
Mijail Q Mariano <mijail.mariano@gmail.com>
2022-08-31 16:14:19 -0600

    311600AUG22 updates

--- acquire_news_articles.py
+++ acquire_news_articles.py
@@ -1,4 +1,5 @@
-# notebook dependencies 
+# notebook dependencies
+import os 
 import pandas as pd
 import numpy as np
 
@@ -6,5 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Beautiful Soup import
+from bs4 import BeautifulSoup
+
 # regular expression import
 import re
+
